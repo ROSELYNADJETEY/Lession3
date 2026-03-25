a9532e049e78293c706ffaf969650d99f062dabd
ROSELYNADJETEY <roselynmingle386@gamil.com>
2026-03-25 06:14:28 -0700

    commit

---
+++ src/script.js
@@ -0,0 +1,43 @@
+console.log("Hello, World!");
+
+
+let username = "Roselyn";
+console.log(username);//Roselyn
+
+console.log("what is your age?");
+let age = prompt("what is your age?");
+console.log(age); 
+
+
+
+
+
+let num1 = 30;
+let num2 = 90;
+// let sum = num1 + num2;
+
+
+console.log(num1 + num2); //num1 + num2 = 120
+console.log(num2 - num1); //num2 - num1 = 60
+console.log(num2 / num1); //num2 / num1 = 3
+console.log(num1 * num2); //num1 * num2 = 2700
+console.log(15 % 4); //15 % 4 = 3
+
+
+let h =true;
+console.log(h); //true
+
+let i = false;
+console.log(i); //false 
+
+let k =  40
+typeof k; //number
+console.log(typeof k); //number 
+
+let l = "60";
+typeof l; //string
+console.log(typeof l); //string
+
+
+
+
