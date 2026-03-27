6444ac91837fa381b672ffea6f00e99c92875158
ROSELYNADJETEY <roselynmingle386@gamil.com>
2026-03-27 06:21:57 -0700

    adding

--- src/script.js
+++ src/script.js
@@ -1,42 +1,42 @@
-// console.log("Hello, World!");
+console.log("Hello, World!");
 
 
-// let username = "Roselyn";
-// console.log(username);//Roselyn
+let username = "Roselyn";
+console.log(username);//Roselyn
 
-// console.log("what is your age?");
-// let age = prompt("what is your age?");
-// console.log(age); 
-
-
+console.log("what is your age?");
+let age = prompt("what is your age?");
+console.log(age); 
 
 
 
-// let num1 = 30;
-// let num2 = 90;
-// let sum = num1 + num2;
 
 
-// console.log(num1 + num2); //num1 + num2 = 120
-// console.log(num2 - num1); //num2 - num1 = 60
-// console.log(num2 / num1); //num2 / num1 = 3
-// console.log(num1 * num2); //num1 * num2 = 2700
-// console.log(15 % 4); //15 % 4 = 3
+let num1 = 30;
+let num2 = 90;
+let sum = num1 + num2;
 
 
-// let h =true;
-// console.log(h); //true
+console.log(num1 + num2); //num1 + num2 = 120
+console.log(num2 - num1); //num2 - num1 = 60
+console.log(num2 / num1); //num2 / num1 = 3
+console.log(num1 * num2); //num1 * num2 = 2700
+console.log(15 % 4); //15 % 4 = 3
 
-// let i = false;
-// console.log(i); //false 
 
-// let k =  40
-// typeof k; //number
-// console.log(typeof k); //number 
+let h =true;
+console.log(h); //true
 
-// let l = "60";
-// typeof l; //string
-// console.log(typeof l); //string
+let i = false;
+console.log(i); //false 
+
+let k =  40
+typeof k; //number
+console.log(typeof k); //number 
+
+let l = "60";
+typeof l; //string
+console.log(typeof l); //string
 
 // //prompt the user to enter a number
 // //check if the number is even or odd 
@@ -44,12 +44,12 @@
 
 
 
-// let number = prompt ("Enter a number: ")
-// if (number % 2 === 0) {
-//    alert(`${number} is even.`);
-// } else {
-//    alert(`${number} is odd.`);
-// }
+let number = prompt ("Enter a number: ")
+if (number % 2 === 0) {
+   alert(`${number} is even.`);
+} else {
+   alert(`${number} is odd.`);
+}
 
 
 // Take the user's name 
@@ -59,13 +59,13 @@
 
 
 
-// let Username = prompt("What is your name?");
-// let answer = prompt("How old are you?");
-// if(answer >= 18) {
-//     alert(`Hello, ${Username}! "You can sign up for tiktok."`);
-// } else {
-//     alert(`Hello, ${Username}. "sorry, You are not old enough to sign up for tiktok."`);
-// }
+let Username = prompt("What is your name?");
+let answer = prompt("How old are you?");
+if(answer >= 18) {
+    alert(`Hello, ${Username}! "You can sign up for tiktok."`);
+} else {
+    alert(`Hello, ${Username}. "sorry, You are not old enough to sign up for tiktok."`);
+}
 
 let name = prompt("what is your name?");
 let age = prompt(" how old are you ");
@@ -82,7 +82,7 @@ if ( age < 12){
         alert(`Hello${name}.You are ${age}. years old and you can register for any option of your choosing.We will keep in touch via your email:${email}. 👋👋 `)
  }  else{
     alert(`You must be 12 years old`)
-    
+
  }
 
 
